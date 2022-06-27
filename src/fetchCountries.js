388832d5dd5bde4fb9c1461583c4c8e2b03d71bd
Wolbu <94459033+Wolbu@users.noreply.github.com>
2022-06-27 22:54:45 +0300

    beta version

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,5 @@
  function fetchCountries(name) {
-    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,official,capital,population,flag,languages`)
+    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,official,capital,population,flags,languages`)
         .then(
             (response) => { 
                 if (!response.ok) {
--- src/index.js
+++ src/index.js
@@ -1,47 +1,40 @@
 import './css/styles.css';
-import { fetchCountries } from './fetchCountries';
+import { fetchCountries} from './fetchCountries';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
-
 const searchInput = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 
 searchInput.addEventListener('input', debounce(inputHandler, DEBOUNCE_DELAY));
 
-
-
-
 function inputHandler() {
     countryList.innerHTML = '';
-    const inputValue = searchInput.value;
-    if (inputValue === "") {
-        Notiflix.Notify.warning('Empty field');
+    const inputValue = searchInput.value.trim();
+    if (inputValue === '') {
+        return;
     } else {
-        fetchCountries(inputValue).then((data) => {
-
-            if (data.length === 1) {
-                const lang = Object.values(data[0].languages)
-                const markup = data.map((country) => {
-                    return `<li>${country.flag} ${country.name.official}</li><p>Capital: ${country.capital}</p><p>Population:${country.population}</p><p>Languages: ${lang}</p>`;
-                })
-                countryList.innerHTML = markup;
-
-
-            } else if (data.length >= 2 && data.length <= 10) {
-                console.log("between 2 and 10")
-                const markup = data.map((country) => {
-                    return `<li>${country.flag} ${country.name.official}</li>`;
-                })
-                countryList.innerHTML = markup;
-            } else {
-                Notiflix.Notify.info('Too many matches found. Please enter a more specific name."');
-            }
-
-        }).catch(()=>{
-            Notiflix.Notify.failure('Oops, there is no country with that name');
-        });
+        fetchCountries(inputValue)
+            .then(data => {
+                if (data.length === 1) {
+                    const lang = Object.values(data[0].languages);
+                    const markup = data.map(country => {
+                        return `<li><h3><img src="${country.flags.svg}" width ="20" alt=""> ${country.name.official}</h3><p>Capital: ${country.capital}</p><p>Population:${country.population}</p <p>Languages: ${lang}</p> </li>`;
+                    });
+                    countryList.innerHTML = markup;
+                } else if (data.length >= 2 && data.length <= 10) {
+                    const markup = data.map(country => {
+                        return `<li><img src="${country.flags.svg}" width ="20" alt=""> ${country.name.official}</li>`});
+                    countryList.innerHTML = markup;
+                } else {
+                    Notiflix.Notify.info(
+                        'Too many matches found. Please enter a more specific name.'
+                    );
+                }
+            })
+            .catch(() => {
+                Notiflix.Notify.failure('Oops, there is no country with that name');
+            });
     }
 }
-
